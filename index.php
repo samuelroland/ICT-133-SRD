90f9a3ff142154d603aa724a00e16810965f0783
samuelroland <samuel.roland@cpnv.ch>
2019-12-01 22:26:14 +0100

    index.php page accueil terminé
    liens exercices automatiques. nouveaux css et msg entete. formatage du nom des exos

--- index.php
+++ index.php
@@ -3,18 +3,43 @@
 <head>
     <meta charset="UTF-8">
     <title>Accueil Exos ICT-133</title>
+    <style>
+        body{
+            text-align: left;
+            font-size: 1.5em;
+            background-color: black;
+            color: white;
+            margin-left: 50px;
+            font-family: Helvetica;
+        }
+        a{
+            color: white;
+            transition: 0.5s;
+        }
+        a:hover{
+            color: blue;
+            transition: 0.5s;
+
+        }
+        a:active{
+            color: mediumvioletred;
+            transition: 1s;
+        }
+        li{
+            color: black;
+            padding: 7px;
+        }
+    </style>
 </head>
 <body>
 <h1>Accueil Exos ICT-133</h1>
-<h2><a href="Exo1_Traduction_C-vers-PHP/index.php">Exo1 Traduction</a></h2>
-<h2><a href="Exo2_Dates/index.php">Exo2 Dates</a></h2>
-<h2><a href="Exo-Liste-personnes/index.php">Exo liste personnes</a></h2>
+<h4>Exercices automatiquement trouvés à la racine du serveur : </h4>
 
 <?php
 $filescollection = scandir($_SERVER['DOCUMENT_ROOT']);  //collection des éléments du dossier qui contient ce fichier.
 $filetoexclude = array(".", "..", ".git", ".gitignore", ".idea", "index.php", "index.html", "images", "img", "js", "javascript","style", "css", "html", "pages", "doc","documentation", "readme.md", "README.md");  //fichier ou dossiers potentiels à exclure. tout le reste sont des dossiers des exercices...
 
-function checkisafolder($folder, $filetoexclude)
+function checkisafolder($folder, $filetoexclude)    //check si l'élément est un dossier, en regardant la liste des fichiers exclus.
 {
     for ($i = 0; $i < count($filetoexclude); $i++) {
         if ($folder == $filetoexclude[$i]) {
@@ -24,12 +49,20 @@ function checkisafolder($folder, $filetoexclude)
     //une fois tout vérifié, si il n'est pas sorti de la fonction --> c'est un dossier d'exercices !
     return true;
 }
+function format($name){ //formater le nom en enlevant les séparateurs.
+    //Si il trouve - ou _ ou . alors il remplace par un espace
+    $name = str_replace("-", " ", $name);
+    $name  = str_replace("_", " ", $name);
+    $name  = str_replace(".", " ", $name);
 
-foreach ($filescollection as $file) {
+    return $name;
+}
+foreach ($filescollection as $file) {   //Pour tous les fichiers trouvés à la racine.
     if (checkisafolder($file, $filetoexclude) == true) {//Si l'élément est un dossier d'exercices
-        //le rajouter à la liste de liens vers les exercices:
+        $formatedname= format($file);   //créer son nom formaté pour le nom de l'exercice à partir du nom du dossier sans les séparateurs.
 
-        echo "<p>$file</p>";    //POUR LES TESTS à SUPPRIMER !
+        //le rajouter à la liste de liens vers les exercices:
+        echo "<li><a href=\"".$file."/index.php\">".$formatedname."</a></li>";
     }
 }
 
