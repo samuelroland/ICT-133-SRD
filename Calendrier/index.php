6234ec86340f6361caa43917f4db2ec295450f88
samuelroland <samuel.roland@cpnv.ch>
2019-12-10 22:06:14 +0100

    Calendrier: reformatage du code
    enlevé conversion à 7 quand 0 le numéro du jour de la semaine...
    ajouté commentaires et supprimés lignes vides et inutiles

--- Calendrier/index.php
+++ Calendrier/index.php
@@ -15,6 +15,12 @@
  */
 
 //Get the data of the query string:
+
+//If month and year are null take this value by default:
+$month = 8;
+$year = 2019;
+//TODO:
+//Comment faire que ca ne plante pas si il n'y a pas de querystring ?
 $month = $_GET['month'];
 $year = $_GET['year'];
 $now = time();  //take the date et time of now
@@ -47,14 +53,11 @@ if ($month == 1) {
 
 $firstdaystartat = date("w", mktime(1, 1, 1, $month, 0, $year));//day of the week that is the 1 of the month
 $firstdaystartat += 0;
-if ($firstdaystartat == 0) {    //si c'est 0 donc dimanche on met sur 7
-    $firstdaystartat = 7;
-}
 
 //Trouver le dernier jour du mois actuel:
-$lastdaypossiblelastmonth = 31;
-while (checkdate($monthbefore, $lastdaypossiblelastmonth, $year) != true) {
-    $lastdaypossiblelastmonth--; //on décremente jusqu'a 28 puisque 28 est le jour le numéro le plus petit pour février
+$lastdaypossiblelastmonth = 31; //par défaut c'est 31 et si c'est pas le cas, il sera réduit en dessous.
+while (checkdate($monthbefore, $lastdaypossiblelastmonth, $year) != true) { //tant que la derniere date du mois précèdent n'est pas une date existante
+    $lastdaypossiblelastmonth--; //on décremente jusqu'a 28 puisque 28 est le jour le numéro le plus petit (utile pour février)
 }
 
 
@@ -63,18 +66,15 @@ while (checkdate($month, $lastdaypossiblethismonth, $year) != true) {
     $lastdaypossiblethismonth--; //on décremente jusqu'a 28 puisque 28 est le jour le numéro le plus petit pour février
 }
 
-//Table of months:
-//$listdays = array("Mo", "Tu", "We", "Thu", "Fr", "Sa", "Sun");
-$month = date("F", mktime(0, 0, 0, $month, 1, 1000));
+//Changement du mois du format 01-12 au format January-December
+$month = date("F", mktime(0, 0, 0, $month, 1, 1000));   //prendre la mois avec une date fictive du bon mois.
 
-
+//Afficher le mois et l'année
 echo '<div class="month"><ul>';
 echo "<li>$month<br>$year</li>";
-
-
 echo "</div></ul>";
 
-echo '<ul class="weekdays">';
+echo '<ul class="weekdays">';   //début des jours de la semaine
 for ($i = 1; $i <= 7; $i++) {
     //Generate days of the week:
     $dayinrun = date("l", mktime(1, 1, 1, 07, $i, 2019));  //juillet 2019 commence un lundi !
@@ -88,10 +88,9 @@ for ($i = 1; $i <= 7; $i++) {
 echo '</ul>';
 
 
-echo '<ul class="days">';
+echo '<ul class="days">';   //début des jours du mois
 for ($i = 1; $i <= $lastdaypossiblethismonth; $i++) {
-    //echo $firstdaystartat;
-    if ($firstdaystartat != 7) {
+    if ($firstdaystartat != 0) {    //Si le premier jour du mois est autre que lundi
         while ($firstdaystartat != 0) {
             $daylastmonth = $lastdaypossiblelastmonth - $firstdaystartat + 1;
             $firstdaystartat -= 1;
@@ -109,11 +108,8 @@ for ($i = 1; $i <= $lastdaypossiblethismonth; $i++) {
 
 echo '</ul>';
 //Boutons mois suivants et précedents:
-
-
 echo "<br><a href='index.php?month=$monthbefore&year=$yeartolast'><div class='button'>Mois précedent</div></a>";
 echo "<br><a href='index.php?month=$monthafter&year=$yeartonext'><div class='button'>Mois suivant</div></a>";
-
 ?>
 
 </body>
