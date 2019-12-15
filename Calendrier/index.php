9e59ab46fe1f15c9c8a814bca30526eee8eab308
samuelroland <samuel.roland@cpnv.ch>
2019-12-15 11:45:51 +0100

    calendrier. presque fini

--- Calendrier/index.php
+++ Calendrier/index.php
@@ -19,16 +19,19 @@
 //If month and year are null take this value by default:
 $month = 8;
 $year = 2019;
+$daysoftheweek = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"]; //listes des jours de la semaine en francais.
 
+//Prendre les valeurs de la querystring si elles sont assignées:
 if (isset($_GET['month']) && isset($_GET['year'])) {
     $month = $_GET['month'];
     $year = $_GET['year'];
 }
 
 $now = time();  //take the date et time of now
-$year += 0;
-$month += 0;
+$year += 0; //conversion string vers int
+$month += 0; //conversion string vers int
 
+//Données pour les boutons:
 //Mois et année, suivants et précedents:
 $monthbefore = $month - 1;  //mois avant
 $monthafter = $month + 1;   //mois après
@@ -37,41 +40,26 @@ $yearafter = $year + 1; //année après
 $yeartonext = $year;    //année lors du clic sur mois suivant
 $yeartolast = $year;    //année lors du clic sur mois précédent
 
-//Si on est au moins 1 ou 12 alors on doit passer à l'année précèdente ou suivante:
-if ($month == 1) {
+//Si on est au mois 1 ou 12 alors on doit passer à l'année précèdente ou suivante:
+if ($month == 1) {  //si c'est janvier
     $monthbefore = 12;
-    $monthafter = 2;
     $yeartolast = $yearbefore;
-} else if ($month == 12) {
+} else if ($month == 12) {  //si c'est décembre
     $monthafter = 1;
-    $monthbefore = 11;
-    $yeartomonth = $yearafter;
     $yeartonext = $yearafter;
-} else {
-    $monthbefore = $month - 1;
-    $monthafter = $month + 1;
-    $yeartomonth = $year;
 }
 
-echo date("Y-M-D", strtotime("$year-$month-0"));
-$firstdaystartat = date("w", strtotime("$year-$month-0"));//day of the week that is the 1 of the month
-$firstdaystartat += 0;
+//day of the week that is the 1 of the month and the year chosen
+$firstdaystartat = date("N", strtotime("$year-$month-01"));
+$firstdaystartat += 0;  //conversion implicite de chaine à entier.
 
-
-//Trouver le dernier jour du mois actuel:
-$lastdaypossiblelastmonth = 31; //par défaut c'est 31 et si c'est pas le cas, il sera réduit en dessous.
-while (checkdate($monthbefore, $lastdaypossiblelastmonth, $year) != true) { //tant que la derniere date du mois précèdent n'est pas une date existante
-    $lastdaypossiblelastmonth--; //on décremente jusqu'a 28 puisque 28 est le jour le numéro le plus petit (utile pour février)
-}
-
-
-$lastdaypossiblethismonth = 31;
-while (checkdate($month, $lastdaypossiblethismonth, $year) != true) {
-    $lastdaypossiblethismonth--; //on décremente jusqu'a 28 puisque 28 est le jour le numéro le plus petit pour février
-}
+//Trouver le dernier jour du mois précédent:
+$lastdaypossiblelastmonth = date("t", strtotime("-1 month", strtotime("$year-$month-01")));
+//Trouver le dernier jour du mois en cours:
+$lastdaypossiblethismonth = date("t", strtotime("$year-$month-01"));;
 
 //Changement du mois du format 01-12 au format January-December
-$month = date("F", mktime(0, 0, 0, $month, 1, 1000));   //prendre la mois avec une date fictive du bon mois.
+$month = date("F", mktime(1, 1, 1, $month, 1, 2000));   //prendre la mois avec une date fictive du bon mois.
 
 //Afficher le mois et l'année
 echo '<div class="month"><ul>';
@@ -81,44 +69,47 @@ echo "</div></ul>";
 echo '<ul class="weekdays">';   //début des jours de la semaine
 for ($i = 1; $i <= 7; $i++) {
     //Generate days of the week:
-    $dayinrun = date("l", mktime(1, 1, 1, 07, $i, 2019));  //juillet 2019 commence un lundi !
-
-    if ($dayinrun == date("l", $now) && $month == date("F", $now) && $year == date("Y", $now)) {
+    //$dayinrun = date("l", mktime(1, 1, 1, 07, $i, 2019));  //juillet 2019 commence un lundi !
+    $dayinrun = $daysoftheweek[$i - 1]; //prendre la liste des jours dans le tableau
+    if ($dayinrun == date("l", $now) && $month == date("F", $now) && $year == date("Y", $now)) {    //si la date c'est maitenant
         echo "<li><span class='active'>$dayinrun</span></li>";
     } else {
         echo "<li>$dayinrun</li>";
     }
 }
 echo '</ul>';
-$nbdayslastmonth = $firstdaystartat; //nombre de jours affichés du mois d'avant.
+$nbdayslastmonth = $firstdaystartat - 1; //nombre de jours affichés du mois d'avant.
 
 echo '<ul class="days">';   //début des jours du mois
 
-//Afficher les numéros des mois précédents si il y en a:
-if ($firstdaystartat != 0) {    //Si le premier jour du mois est autre que lundi
-    while ($firstdaystartat != 0) {
-        $daylastmonth = $lastdaypossiblelastmonth - $firstdaystartat + 1;
-        $firstdaystartat -= 1;
-        echo "<li class='lastmonth'>$daylastmonth</li>";
-    }
+//Afficher les jours du mois précédent si il y en a:
+for ($i = $nbdayslastmonth % 7; $i > 0; $i--) {
+    $daytoprint = $lastdaypossiblelastmonth - $i + 1;   //pour trouver 28, 29, 30, 31, ...
+    echo "<li class='othermonth'>$daytoprint</li>";
+    $calendercontent[count($calendercontent) + 1] = $daytoprint;   //rajouter la valeur au contenu du calendrier
 }
 
 //Afficher les jours du mois courant:
 for ($i = 1; $i <= $lastdaypossiblethismonth; $i++) {
-
-
     if ($i == date("j", $now) && $month == date("F", $now) && $year == date("Y", $now)) {
         echo "<li class='thismonth'><span class='active'>$i</span></li>";
     } else {
         echo "<li class='thismonth'>$i</li>";
     }
+    $calendercontent[count($calendercontent) + 1] = $i;   //rajouter la valeur au contenu du calendrier
 }
 
-//Afficher les jours des mois d'après si il y en a:
+//Afficher les jours du mois suivant si il y en a:
+$nbdaysnextmonth = (7 - (($nbdayslastmonth + $lastdaypossiblethismonth) % 7)) % 7;  //nombre de case à remplir pour atteindre le bout de la dernière ligne du tableau.
+for ($j = 1; $j <= $nbdaysnextmonth; $j++) {
+    echo "<li class='othermonth'>$j</li>";
+    $calendercontent[count($calendercontent) + 1] = $j;   //rajouter la valeur au contenu du calendrier
+}
 
+echo '</ul>';   //fin de la liste du tableau.
+//var_dump($calendercontent);   //observer la variable pour voir le contenu du tableau
 
-echo '</ul>';
-//Boutons mois suivants et précedents:
+//Boutons mois suivant et précedent:
 echo "<br><a href='index.php?month=$monthbefore&year=$yeartolast'><div class='button'>Mois précedent</div></a>";
 echo "<br><a href='index.php?month=$monthafter&year=$yeartonext'><div class='button'>Mois suivant</div></a>";
 ?>
