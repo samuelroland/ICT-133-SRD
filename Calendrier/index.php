f4b6a77b239b098233baf8129f8ffaa7167d42fd
samuelroland <samuel.roland@cpnv.ch>
2019-12-16 22:49:30 +0100

    Calendrier: refactorisation importante
    correction bug active sur jour semaine
    ajout de 4 fonctions pour générer le contenu du tableau séparement. (objectifs d'eva).

--- Calendrier/index.php
+++ Calendrier/index.php
@@ -49,62 +49,156 @@ if ($month == 1) {  //si c'est janvier
     $yeartonext = $yearafter;
 }
 
-//day of the week that is the 1 of the month and the year chosen
-$firstdaystartat = date("N", strtotime("$year-$month-01"));
-$firstdaystartat += 0;  //conversion implicite de chaine à entier.
-
-//Trouver le dernier jour du mois précédent:
-$lastdaypossiblelastmonth = date("t", strtotime("-1 month", strtotime("$year-$month-01")));
-//Trouver le dernier jour du mois en cours:
-$lastdaypossiblethismonth = date("t", strtotime("$year-$month-01"));;
-
-//Changement du mois du format 01-12 au format January-December
+//Changement du mois du format 01-12 au format January-December pour afficher
 $month = date("F", mktime(1, 1, 1, $month, 1, 2000));   //prendre la mois avec une date fictive du bon mois.
 
+//Fonctions:
+/**
+ * La fonction getDaysBefore retourne une chaîne de caractères qui représente les jours du
+ * début de la semaine qui précèdent le premier jour du mois donné.
+ * Cette chaîne peut donc être vide si le mois choisi commence un lundi.
+ * Chaque jour est mis dans une balise <li> qui a la classe CSS 'discreet'
+ *
+ * Exemple:
+ *     Le mois d'avril 2020 commence un mercredi, il est précédé du mois de mars, qui compte 31 jours.
+ *     On doit donc voir le lundi 30 mars et le mardi 31 mars avant le mercredi 1er avril
+ *     getDaysBefore(4,2020) retournera donc la chaîne:
+ *
+ *     <li class='discreet'>30</li><li class='discreet'>31</li>
+ *
+ * @param $month , numérique: le mois à afficher
+ * @param $year , numérique: l'année du mois à afficher
+ * @return $res , string : listes des jours d'avant
+ */
+
+function getDaysBefore($month, $year)
+{
+    //day of the week that is the 1 of the month and the year chosen
+    $firstdaystartat = date("N", strtotime("$year-$month-01"));
+    $firstdaystartat += 0;  //conversion implicite de chaine à entier.
+
+    //Trouver le dernier jour du mois précédent:
+    $lastdaypossiblelastmonth = date("t", strtotime("-1 month", strtotime("$year-$month-01")));
+    $nbdayslastmonth = $firstdaystartat - 1; //nombre de jours affichés du mois d'avant.
+
+    $res = "";
+    //Afficher les jours du mois précédent si il y en a:
+    for ($i = $nbdayslastmonth % 7; $i > 0; $i--) {
+        $daytoprint = $lastdaypossiblelastmonth - $i + 1;   //pour trouver 28, 29, 30, 31, ...
+        $res .= "<li class='othermonth'>$daytoprint</li>";
+        $calendercontent[count($calendercontent) + 1] = $daytoprint;   //rajouter la valeur au contenu du calendrier
+    }
+    return $res;
+}
+
+/**
+ * La fonction getDaysAfter retourne une chaîne de caractères qui représente les jours de
+ * la fin de la semaine qui suivent le dernier jour du mois donné.
+ * Cette chaîne peut donc être vide si le mois choisi se termine un dimanche.
+ * Chaque jour est mis dans une balise <li> qui a la classe CSS 'discreet'
+ *
+ * Exemple:
+ *     Le mois d'avril 2020 finit un jeudi.
+ *     On doit donc voir le vendredi 1er mai, le samedi 2 mai et le dimanche 3 mai après le jeudi 30 avril
+ *     getDaysAfter(4,2020) retournera donc la chaîne:
+ *
+ *     <li class='discreet'>1</li><li class='discreet'>2</li><li class='discreet'>3</li>
+ *
+ * @param $month , numérique: le mois à afficher
+ * @param $year , numérique: l'année du mois à afficher
+ * @return $res , string : listes des jours d'après
+ */
+
+function getDaysAfter($month, $year)
+{
+    //day of the week that is the 1 of the month and the year chosen
+    $firstdaystartat = date("N", strtotime("$year-$month-01"));
+    $firstdaystartat += 0;  //conversion implicite de chaine à entier.
+
+    //Trouver le dernier jour du mois précédent:
+    $lastdaypossiblelastmonth = date("t", strtotime("-1 month", strtotime("$year-$month-01")));
+    $nbdayslastmonth = $firstdaystartat - 1; //nombre de jours affichés du mois d'avant.
+
+    //Trouver le dernier jour du mois en cours:
+    $lastdaypossiblethismonth = date("t", strtotime("$year-$month-01"));;
+
+    $res = "";
+    //Afficher les jours du mois suivant si il y en a:
+    $nbdaysnextmonth = (7 - (($nbdayslastmonth + $lastdaypossiblethismonth) % 7)) % 7;  //nombre de case à remplir pour atteindre le bout de la dernière ligne du tableau.
+    for ($j = 1; $j <= $nbdaysnextmonth; $j++) {
+        $res .= "<li class='othermonth'>$j</li>";
+        $calendercontent[count($calendercontent) + 1] = $j;   //rajouter la valeur au contenu du calendrier
+    }
+    return $res;
+}
+
+/**
+ * La fonction getCalendarContent rend une chaîne de caractères qui représente tous les jours du mois en cours
+ * Cette chaine ne peut pas être nulle
+ * Chaque jour est mis dans une balise <li> qui a la classe CSS 'thismont'
+ *
+ * @param $month , numérique: le mois à afficher
+ * @param $year , numérique: l'année du mois à afficher
+ * @return $res , string : listes des jours du mois en cours
+ */
+
+function getCalendarContent($month, $year)
+{
+    $res = "";
+    echo getDaysBefore($month, $year);
+    //Trouver le dernier jour du mois en cours:
+    $lastdaypossiblethismonth = date("t", strtotime("$year-$month-01"));;
+    //Afficher les jours du mois courant:
+    for ($i = 1; $i <= $lastdaypossiblethismonth; $i++) {
+        if ($i == date("j") && $month == date("F") && $year == date("Y")) {
+            echo "<li class='thismonth'><span class='active'>$i</span></li>";
+        } else {
+            echo "<li class='thismonth'>$i</li>";
+        }
+        $calendercontent[count($calendercontent) + 1] = $i;   //rajouter la valeur au contenu du calendrier
+    }
+    echo getDaysAfter($month, $year);
+    return $res;
+}
+
+/**
+ * La fonction getHeader rend une chaîne de caractères qui représente tous les jours du mois en cours
+ * Cette chaine ne peut pas être nulle
+ * Chaque jour est mis dans une balise <li> qui a la classe CSS 'thismont'
+ *
+ * @param $month , numérique: le mois à afficher
+ * @param $year , numérique: l'année du mois à afficher
+ * @return $res , string : listes des jours du mois en cours
+ */
+function getHeader($month, $year)
+{
 //Afficher le mois et l'année
-echo '<div class="month"><ul>';
-echo "<li>$month<br>$year</li>";
-echo "</div></ul>";
+    $res = "";
+    $res .= '<div class="month"><ul>';
+    $res .= "<li>$month<br>$year</li>";
+    $res .= "</div></ul>";
+    return $res;
+}
+
+//Début de la page:
+echo getHeader($month, $year);
 
 echo '<ul class="weekdays">';   //début des jours de la semaine
 for ($i = 1; $i <= 7; $i++) {
     //Generate days of the week:
     //$dayinrun = date("l", mktime(1, 1, 1, 07, $i, 2019));  //juillet 2019 commence un lundi !
     $dayinrun = $daysoftheweek[$i - 1]; //prendre la liste des jours dans le tableau
-    if ($dayinrun == date("l", $now) && $month == date("F", $now) && $year == date("Y", $now)) {    //si la date c'est maitenant
+    if ($i == date("N") && $month == date("F") && $year == date("Y")) {    //si la date c'est maintenant
         echo "<li><span class='active'>$dayinrun</span></li>";
     } else {
         echo "<li>$dayinrun</li>";
     }
 }
 echo '</ul>';
-$nbdayslastmonth = $firstdaystartat - 1; //nombre de jours affichés du mois d'avant.
 
 echo '<ul class="days">';   //début des jours du mois
 
-//Afficher les jours du mois précédent si il y en a:
-for ($i = $nbdayslastmonth % 7; $i > 0; $i--) {
-    $daytoprint = $lastdaypossiblelastmonth - $i + 1;   //pour trouver 28, 29, 30, 31, ...
-    echo "<li class='othermonth'>$daytoprint</li>";
-    $calendercontent[count($calendercontent) + 1] = $daytoprint;   //rajouter la valeur au contenu du calendrier
-}
-
-//Afficher les jours du mois courant:
-for ($i = 1; $i <= $lastdaypossiblethismonth; $i++) {
-    if ($i == date("j", $now) && $month == date("F", $now) && $year == date("Y", $now)) {
-        echo "<li class='thismonth'><span class='active'>$i</span></li>";
-    } else {
-        echo "<li class='thismonth'>$i</li>";
-    }
-    $calendercontent[count($calendercontent) + 1] = $i;   //rajouter la valeur au contenu du calendrier
-}
-
-//Afficher les jours du mois suivant si il y en a:
-$nbdaysnextmonth = (7 - (($nbdayslastmonth + $lastdaypossiblethismonth) % 7)) % 7;  //nombre de case à remplir pour atteindre le bout de la dernière ligne du tableau.
-for ($j = 1; $j <= $nbdaysnextmonth; $j++) {
-    echo "<li class='othermonth'>$j</li>";
-    $calendercontent[count($calendercontent) + 1] = $j;   //rajouter la valeur au contenu du calendrier
-}
+echo getCalendarContent($month, $year);
 
 echo '</ul>';   //fin de la liste du tableau.
 //var_dump($calendercontent);   //observer la variable pour voir le contenu du tableau
