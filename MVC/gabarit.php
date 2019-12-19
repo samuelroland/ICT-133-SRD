f177cd9631f33a5dc21a4808a4d4f23b9a389e4a
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2019-12-19 10:16:25 +0100

    import MVC exo

---
+++ MVC/gabarit.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Titre</title>
+    <script src="/node_modules/jquery/dist/jquery.js"></script>
+    <link rel="stylesheet" href="/node_modules/bootstrap/dist/css/bootstrap.css">
+    <script src="/node_modules/bootstrap/dist/js/bootstrap.js"></script>
+    <link rel="stylesheet" href="/css/styles.css">
+</head>
+<body>
+<header class="text-center">
+    <img src="/images/CPNVinfo.png" width="100px">
+</header>
+<div class="container">
+Contenu
+</div>
+<footer class="text-center">
+    <img src="/images/fb.png" width="20px" height="20px">
+    <img src="/images/twitter.png" width="20px" height="20px">
+    <img src="/images/insta.png" width="20px" height="20px">
+    <img src="/images/snap.png" width="20px" height="20px">
+</footer>
+</body>
+</html>
